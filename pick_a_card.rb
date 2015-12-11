e187f437f1806ae342e4713ff3b02ad56000a1d6
Vasol <nobix@rambler.ru>
2015-12-11 23:47:38 +0400

    Update pick_a_card.rb

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,5 @@
 # encoding: UTF-8
 values = ['2','3','5','6','7','Q','K']
-suits = ['Diamonds','Hearts','Clubs','Spades']
-
+suits = ['♦','♥','♣','♠']
+#chsnged symbols
 puts "#{values.sample} of #{suits.sample}"
