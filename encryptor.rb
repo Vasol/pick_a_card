0a4ff1e3abfaa0f5650be3d10bbe430d19a687b7
ascolta <mail@ascolta.ru>
2015-12-11 23:41:00 +0400

    version with SHA3

--- encryptor.rb
+++ encryptor.rb
@@ -4,12 +4,14 @@ puts "Введите слово или фразу для шифрования:"
 
 slovo = gets.chomp
 
-puts "Каким способом зашифровать: \n 1. MD5 \n 2. SHA1"
+puts "Каким способом зашифровать: \n 1. MD5 \n 2. SHA1 \n 3. SHA2"
 
 metod = gets.chomp
 
 if metod == 1
   puts "Вот что получилось: \n #{Digest::MD5.hexdigest.(slovo)}"
-else
+elsif metod == 2
   puts "Вот что получилось: \n #{Digest::SHA1.hexdigest(slovo)}"
+else
+  puts "Вот что получилось: \n #{Digest::SHA2.hexdigest(slovo)}"
 end
